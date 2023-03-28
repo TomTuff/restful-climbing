0021e688e91c0ab0fe7e84440b3363ee49be0f09
Tom Kaufman <tommyk1995@gmail.com>
2023-03-28 15:59:08 -0400

    Complete get_recent_climbers(), ... add_new_climber(), delete_climber()

--- src/climbers.rs
+++ src/climbers.rs
@@ -1,28 +1,91 @@
-use crate::pg::conn;
 use crate::climber::{Climber, NumberClimbers};
+use crate::pg::conn;
 use actix_web::{delete, get, post, put, web, HttpResponse, Responder};
 use log::error;
 use sqlx::{self, query};
 
 #[get("")]
-async fn get_recent_climbers() -> impl Responder {
-    HttpResponse::ExpectationFailed()
+async fn get_recent_climbers(number_climbers: Option<web::Json<NumberClimbers>>) -> impl Responder {
+    let number_of_climbers_to_request = match number_climbers {
+        Some(n) => n.0.number_climbers,
+        None => 5,
+    };
+
+    if let Ok(mut conn) = conn().await {
+        if let Ok(query_result) = query!(
+            r#"SELECT id as "id!", username as "username!" FROM climbers ORDER BY created_at DESC LIMIT ($1)"#,
+            number_of_climbers_to_request,
+        )
+        .fetch_all(&mut conn)
+        .await
+        {
+            let climbers = query_result
+                .iter()
+                .map(|record| Climber {
+                    id: Some(record.id),
+                    username: record.username.to_owned(),
+                })
+                .collect::<Vec<Climber>>();
+            HttpResponse::Ok().json(climbers)
+        } else {
+            error!("SELECT query failed in get_recent_climbers()");
+            HttpResponse::BadGateway().finish()
+        }
+    } else {
+        error!("Failed to connect to the database in get_recent_climbers()");
+        HttpResponse::BadGateway().finish()
+    }
 }
 
 #[post("")]
-async fn add_new_climber() -> impl Responder {
-    HttpResponse::ExpectationFailed()
+async fn add_new_climber(json: web::Json<Climber>) -> impl Responder {
+    if let Ok(mut conn) = conn().await {
+        if let Ok(_query_result) = query!(
+            "INSERT INTO climbers (username) VALUES ($1)",
+            json.0.username,
+        )
+        .execute(&mut conn)
+        .await
+        {
+            HttpResponse::Ok()
+        } else {
+            error!("INSERT query failed in add_new_climber()");
+            HttpResponse::BadGateway()
+        }
+    } else {
+        error!("Failed to connect to the database in add_new_climber()");
+        HttpResponse::BadGateway()
+    }
 }
 
-#[get("/climbers/{id}")]
+#[get("/{id}")]
 async fn get_climber_recent_climbs() -> impl Responder {
     HttpResponse::ExpectationFailed()
 }
 
-#[delete("/climbers/{id}")]
-async fn delete_climber() -> impl Responder {
-    HttpResponse::ExpectationFailed()
+#[delete("/{id}")]
+async fn delete_climber(path: web::Path<i32>) -> impl Responder {
+    let id = path.into_inner();
+    if let Ok(mut conn) = conn().await {
+        if query!(
+            r#"DELETE FROM climbers WHERE id = ($1)"#,
+            id,
+        )
+        .execute(&mut conn)
+        .await
+        .is_ok()
+        {
+            HttpResponse::Ok()
+        } else {
+            error!("DELETE query failed in delete_climber()");
+            HttpResponse::BadGateway()
+        }
+    } else {
+        error!("Failed to connect to the database in delete_climber()");
+        HttpResponse::BadGateway()
+    }
 }
+
 
 #[get("/climbers/{climber_id}/{route_id}")]
 async fn get_climbers_review_by_route_id() -> impl Responder {
